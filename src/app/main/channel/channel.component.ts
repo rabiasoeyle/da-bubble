f0122659ab4210a42c222f5489ec15cc3c0e1baa
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2025-03-06 16:40:33 +0100

    <feat>:<channel details of channel in component finished>

--- src/app/main/channel/channel.component.ts
+++ src/app/main/channel/channel.component.ts
@@ -24,22 +24,22 @@ import { DetailsOfChannelComponent } from './details-of-channel/details-of-chann
 export class ChannelComponent implements OnInit, OnChanges{
   userData: UserData |null = null;
   fb = inject(FormBuilder);
-  addChannelForm = this.fb.nonNullable.group({
-    channelname:['', Validators.required],
-    description:['', Validators.required],
-  })
+  // addChannelForm = this.fb.nonNullable.group({
+  //   channelname:['', Validators.required],
+  //   description:['', Validators.required],
+  // })
 sendMessageForm = this.fb.nonNullable.group({
   message:['', Validators.required],
   })
-changeChannelNameForm = this.fb.nonNullable.group({
-  name:['', Validators.required],
-})
-changeChannelDescrForm =this.fb.nonNullable.group({
-  description:['', Validators.required],
-})
-editMessageForm= this.fb.nonNullable.group({
-  message:['', Validators.required],
-  })
+// changeChannelNameForm = this.fb.nonNullable.group({
+//   name:['', Validators.required],
+// })
+// changeChannelDescrForm =this.fb.nonNullable.group({
+//   description:['', Validators.required],
+// })
+// editMessageForm= this.fb.nonNullable.group({
+//   message:['', Validators.required],
+//   })
   @Input()currentChannelName:string = "kein Name";
   openDetailsOfChannel= false;
   editChannel=false;
@@ -54,22 +54,6 @@ editMessageForm= this.fb.nonNullable.group({
   @Output() openChat = new EventEmitter<number>();
   constructor(private chatService:ChatService, private authService:AuthService, private userService:UserService){
   }
-// isSameDay(timestamp1: number, timestamp2: number): boolean {
-//     const date1 = new Date(timestamp1);
-//     const date2 = new Date(timestamp2);
-    
-//     return date1.getFullYear() === date2.getFullYear() &&
-//            date1.getMonth() === date2.getMonth() &&
-//            date1.getDate() === date2.getDate();
-// }
-// formatDate(timestamp: number): string {
-//     return new Date(timestamp).toLocaleDateString("de-DE", {
-//         weekday: "long",
-//         day: "2-digit",
-//         month: "long",
-//         year: "numeric",
-//     });
-// }
   ngOnChanges(changes: SimpleChanges) {
     if (changes['currentChannelName']) {
       this.openChannel(this.currentChannelName);
@@ -110,24 +94,6 @@ editMessageForm= this.fb.nonNullable.group({
     }});
     return this.currentChannel;
   }
-  // startEditMessage(id:number){
-  //   if(this.currentChannel != null){
-  //     const msg = this.currentChannel.messages[id];
-  //     msg.editing = true;
-  //   }
-  // }
-  // closeEditMessage(id:number){
-  //   if(this.currentChannel!=null){
-  //     const msg = this.currentChannel.messages[id];
-  //     msg.editing = false;
-  //   }
-  // }
-  // editMessage(id:number){
-  //   const rawForm = this.editMessageForm.getRawValue();
-  //   if(this.currentChannel && rawForm.message !=""){
-  //     this.chatService.editMessage(this.currentChannel.name, rawForm.message, id);
-  //   }
-  // }
   sendMessage(){
     const rawForm = this.sendMessageForm.getRawValue();
       if(this.currentChannel){
@@ -139,11 +105,8 @@ editMessageForm= this.fb.nonNullable.group({
   }
   addMembersToChannelDialog(){
     this.membersOfChannelList=false;
-      this.addMemberToChannel =!this.addMemberToChannel;
+    this.addMemberToChannel =!this.addMemberToChannel;
   }
-  // addMembersToChannel(channelname:string){
-  //   this.chatService.addMembersToChannel(channelname, this.addMemberData.name);
-  // }
   openMembersList(){
     this.membersOfChannelList = !this.membersOfChannelList;
   }
@@ -182,34 +145,15 @@ editMessageForm= this.fb.nonNullable.group({
   openEditChannel(){
     this.editChannel = !this.editChannel;
   }
-  deleteChannelAtUser(channelname:string){
-    this.chatService.deleteChannelAtMember(channelname);
-      if(this.editChannel){
-        this.editChannel=false;
-      }
-      this.openDetailsAboutChannel();
+  deleteChannelAtUser(){
       if(this.currentChannel){
         this.deletedChannelname = this.currentChannel.name;
       }
       this.channelDeleted = true;
       this.currentChannel = null;
       this.chatService.unsubscribeFromChannel();
-  }
-  changeChannelName(){
-    const rawForm = this.changeChannelNameForm.getRawValue();
-      if(this.currentChannel && rawForm.name !=""){
-        this.chatService.changeChannelName(this.currentChannel.name, rawForm.name);
-        this.chatService.changeChannelNameForUsers(this.currentChannel.name, rawForm.name);
-      }
       setTimeout(()=>{
-        this.openEditChannel(),this.openChannel(rawForm.name), this.openDetailsAboutChannel()
-      },200);
-  }
-  changeChannelDescription(){
-    const rawForm = this.changeChannelDescrForm.getRawValue();
-      if(this.currentChannel && rawForm.description !=""){
-      this.chatService.changeChannelDescription(this.currentChannel.name, rawForm.description);
-      }
-      this.openEditChannel();
+        this.channelDeleted=false;
+      },4000)
   }
 }
--- src/app/main/channel/details-of-channel/details-of-channel.component.ts
+++ src/app/main/channel/details-of-channel/details-of-channel.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Channel } from '../../../../modules/channel';
 import { FormsModule } from '@angular/forms';
+import { ChatService } from '../../../chat.service';
 
 @Component({
   selector: 'app-details-of-channel',
@@ -16,14 +17,44 @@ export class DetailsOfChannelComponent {
     name:"",
     description:""
   }
+  @Output() closeDetailsEvent = new EventEmitter<number>();
+  @Output() deleteChannelEvent = new EventEmitter<void>();
+  @Output() openChannelEvent = new EventEmitter<string>();
+  isNameChanged = false;
+  isDescriptionChanged = false;
+  deletedChannelname="";
+  channelDeleted=true;
+
+  constructor(private chatService:ChatService){}
+
   closeDetailsAboutChannel(){
-  
+  this.closeDetailsEvent.emit();
   }
   openEditChannel(){
     this.editChannel = !this.editChannel;
   }
-  deleteChannelAtUser(name:string){}
-  changeChannelName(){}
-  changeChannelDescription(){}
-
+  deleteChannelAtUser(){
+    if(this.currentChannel){
+    this.chatService.deleteChannelAtMember(this.currentChannel.name);
+      if(this.editChannel){
+        this.editChannel=false;
+      }
+      this.closeDetailsEvent.emit();
+      this.deleteChannelEvent.emit();
+  }}
+  changeChannelName(){
+    if(this.currentChannel ){
+    this.chatService.changeChannelName(this.currentChannel.name, this.channelData.name);
+    this.chatService.changeChannelNameForUsers(this.currentChannel.name, this.channelData.name);}
+    setTimeout(()=>{
+            this.openEditChannel(),
+            this.openChannelEvent.emit(this.channelData.name) 
+            // this.closeDetailsEvent.emit()
+      },200);
+  }
+  changeChannelDescription(){
+    if(this.currentChannel){
+    this.chatService.changeChannelDescription(this.currentChannel.name, this.channelData.description);
+    }this.openEditChannel();
+  }
 }
