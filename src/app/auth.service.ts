196faa710a1f7b1a386166279477a9fda73ffc66
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2025-04-09 10:38:25 +0200

    <feat>:<getAuth statt inject(Auth)>

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, OnInit} from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from '@angular/fire/auth';
-import { doc, Firestore, getDoc, onSnapshot, setDoc} from '@angular/fire/firestore';
+import { doc, Firestore, getDoc, getFirestore, onSnapshot, setDoc} from '@angular/fire/firestore';
 import { BehaviorSubject, catchError, from, Observable, of, switchMap, throwError } from 'rxjs';
 import { signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider,confirmPasswordReset, onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, User, verifyPasswordResetCode, UserCredential, browserLocalPersistence, setPersistence, fetchSignInMethodsForEmail } from "firebase/auth";
   // import { GoogleAuthProvider, signInWithPopup,signInWithRedirect, getRedirectResult } from "@angular/fire/auth";
@@ -10,8 +10,8 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { getAuth } from "firebase/auth";
 import { getApps, initializeApp } from "firebase/app";
+import { getDatabase } from '@angular/fire/database';
 
-// import { , User } from "firebase/auth";
 
 @Injectable({
   providedIn: 'root'
@@ -28,8 +28,9 @@ export class AuthService{
   };
   app = initializeApp(this.appConfig);
   auth = getAuth();
+  // fd=getFirestore();
+  // fd= getDatabase()
   firebase = inject(FirebaseApp);
-  firebaseAuth = inject(Auth);
   firebaseDatabase = inject(Firestore);
   private userDataSubject = new BehaviorSubject<UserData | null>(null); 
   public userData$ = this.userDataSubject.asObservable();
@@ -37,7 +38,7 @@ export class AuthService{
   private unsubscribeUserUpdates!: () => void;
   router = inject(Router);
   constructor() {
-    onAuthStateChanged(this.firebaseAuth, (user) => {
+    onAuthStateChanged(this.auth, (user) => {
       if (user) {
           this.getData(user.uid).then((data) => {
               this.userDataSubject.next(data);
@@ -53,12 +54,12 @@ export class AuthService{
   }
   register(email: string, name: string, password: string): Observable<void> {
     const checkEmailAndCreateAccount = () => {
-      return from(fetchSignInMethodsForEmail(this.firebaseAuth, email)).pipe(
+      return from(fetchSignInMethodsForEmail(this.auth, email)).pipe(
         switchMap((signInMethods) => {
           if (signInMethods && signInMethods.length > 0) {
             return throwError(new Error('E-Mail-Adresse bereits registriert.'));
           } else {
-            return from(createUserWithEmailAndPassword(this.firebaseAuth, email, password)).pipe(
+            return from(createUserWithEmailAndPassword(this.auth, email, password)).pipe(
               switchMap(async (response) => {
                 const user = response.user;
                 if (user) {
@@ -96,7 +97,7 @@ export class AuthService{
     }); 
   }
   login(email: string, password: string): Observable<void> {
-    const loginPromise = signInWithEmailAndPassword(this.firebaseAuth, email, password)
+    const loginPromise = signInWithEmailAndPassword(this.auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
       if (user) {
@@ -111,34 +112,30 @@ export class AuthService{
     return from(loginPromise);
   }
   async changePresenceStatus(status: boolean, uid:string) {
-    // const uid = localStorage.getItem('userId');
     if (uid) {
       try {
         const userDocRef = doc(this.firebaseDatabase, `users/${uid}`);
         await setDoc(userDocRef, { presenceStatus: status }, { merge: true });
-        console.log(`Presence status for user ${uid} updated to ${status}`);
       } catch (error) {
-        console.error('Fehler beim Aktualisieren des statuses', error);
       }
-    } else {
-      console.error('userId nicht im localStorage gefunden.');
-    }
+    } 
+    // else {
+    //   console.error('userId nicht im localStorage gefunden.');
+    // }
   }
   logout(): void {
     const uid = localStorage.getItem('userId');
     setTimeout(()=>{
     if(uid){this.changePresenceStatus(false, uid)}},1000)
     localStorage.removeItem("userId");
-    this.firebaseAuth.signOut();
+    this.auth.signOut();
     this.userDataSubject.next(null);
     
   }
   async googleSignin(): Promise<void> {
     try {
-        console.log("🚀 googleSignin() wurde aufgerufen!");
         const provider = new GoogleAuthProvider();
-        const auth = getAuth();
-        signInWithPopup(auth,provider)
+        signInWithPopup(this.auth,provider)
           .then((result) => {
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential?.accessToken;
@@ -150,8 +147,6 @@ export class AuthService{
               token: token,
             }
             this.loginNext(user)
-            
-            console.log(googleParam)
           })
             } catch (error) {
                 console.error("❌ Fehler beim Google Login:", error);
@@ -159,14 +154,12 @@ export class AuthService{
 }
 async loginNext(user:any){
       if (user && user.email) {
-          console.log("Google-Login User gefunden");
           const userDocRef = doc(this.firebaseDatabase, "users", user.uid);
           const userDoc = await getDoc(userDocRef);
           if (!userDoc.exists()) {
               await this.addData(user.uid, user.email, user.displayName);
           }
           localStorage.setItem("userId", user.uid);
-          console.log("User-ID gespeichert:", localStorage.getItem("userId"));
           const data = await this.getData(user.uid);
       }
 }
@@ -176,25 +169,23 @@ async loginNext(user:any){
       url: 'http://localhost:4200/changePassword',
       handleCodeInApp: true,
     };
-    sendPasswordResetEmail(this.firebaseAuth, email, actionCodeSettings)
+    sendPasswordResetEmail(this.auth, email, actionCodeSettings)
     .then(()=>{
       alert("Your Password reset link was send")
     })
   }
   // Passwort-Zurücksetzungslink validieren
   verifyResetCode(oobCode: string): Promise<void> {
-    return verifyPasswordResetCode(this.firebaseAuth, oobCode)
+    return verifyPasswordResetCode(this.auth, oobCode)
       .then(() => Promise.resolve()) // Link ist gültig
       .catch((error) => {
-        console.error('Invalid or expired reset link:', error);
         return Promise.reject(error); // Fehler bei der Validierung
       });
   }
   // Neues Passwort setzen
   resetPassword(oobCode: string, newPassword: string): Promise<void> {
-    return confirmPasswordReset(this.firebaseAuth, oobCode, newPassword)
+    return confirmPasswordReset(this.auth, oobCode, newPassword)
       .then(() => {
-        alert('Password successfully changed');
       })
       .catch((error) => {
         console.error('Error resetting password:', error);
